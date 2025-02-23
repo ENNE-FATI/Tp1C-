f961bd48e9290ca31b67aa9f9f8ec193aa64d4af
ENNE-FATI <ennesyryfatimazahra5@gmail.com>
2025-02-23 20:09:15 +0100

    Exercice7.cpp

--- Exercice7.cpp
+++ Exercice7.cpp
@@ -1,46 +1,38 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main() {
-    int L, C;
+    int nombreLignes;
+    cout << "Entrez le nombre de lignes : ";
+    cin >> nombreLignes;
 
-    cout << "Le nombre de lignes : ";
-    cin >> L;
+    int taillesLignes[nombreLignes];
+    int tableau[nombreLignes][100];
 
-    vector<vector<int>> tab(L);
-
-    // Saisie de la matrice irrégulière
-    for (int i = 0; i < L; i++) {
-        cout << "Le nombre des colonnes pour la ligne " << i << " : ";
-        cin >> C;
-
-        tab[i].resize(C); // Allouer l'espace pour cette ligne
-
-        for (int j = 0; j < C; j++) {
-            cout << "Élément [" << i << "][" << j << "] : ";
-            cin >> tab[i][j];
+    for (int i = 0; i < nombreLignes; i++) {
+        cout << "Entrez la taille de la ligne " << (i + 1) << " : ";
+        cin >> taillesLignes[i];
+        cout << "Entrez les elements de la ligne " << (i + 1) << " : ";
+        for (int j = 0; j < taillesLignes[i]; j++) {
+            cin >> tableau[i][j];
         }
     }
 
-    // Détermination de la ligne avec la somme maximale
     int maxSomme = 0;
-    int indexMax = 0;
+    int ligneMax = 0;
 
-    for (int i = 0; i < L; i++) {
+    for (int i = 0; i < nombreLignes; i++) {
         int somme = 0;
-        for (int j = 0; j < tab[i].size(); j++) {
-            somme += tab[i][j];
+        for (int j = 0; j < taillesLignes[i]; j++) {
+            somme += tableau[i][j];
         }
         if (somme > maxSomme) {
             maxSomme = somme;
-            indexMax = i;
+            ligneMax = i + 1;
         }
     }
 
-    cout << "La ligne avec la plus grande somme est : " << indexMax
-         << " avec une somme de " << maxSomme << endl;
-
+    cout << "La ligne avec la plus grande somme est la ligne " << ligneMax << " avec une somme de " << maxSomme;
     return 0;
 }
