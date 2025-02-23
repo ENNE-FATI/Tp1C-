eeafe373f785da9aa9673767b10bd72cee06335c
ENNE-FATI <ennesyryfatimazahra5@gmail.com>
2025-02-23 20:06:22 +0100

    Exercice3.cpp

--- Exercice3.cpp
+++ Exercice3.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 int main() {
     int taille;
-
-    cout << "Entrez la taille du tableau: ";
+    cout << "Entrez la taille du tableau : ";
     cin >> taille;
+    int tab[taille];
 
-    vector<int> tab(taille);
-
+    cout << "Entrez les elements du tableau :";
     for (int i = 0; i < taille; i++) {
-        cout << "L'element de " << i << " est: ";
+        cout << "Donnez l'element n° " << i << " : ";
         cin >> tab[i];
     }
 
     int j = 0;
-    for (int i = 0; i < taille; i++) {
-        if (tab[i] % 2 == 0) {
-            swap(tab[i], tab[j]);
-            j++;
-        }
-    }
+	for (int i = 0; i < taille; i++) {
+		if (tab[i] % 2 == 0) {
+			int temp = tab[i];
+			tab[i] = tab[j];
+			tab[j] = temp;
+			j++;
+		}
+	}
 
-    cout << "Tableau modifie : " << endl;
+    cout << "Tableau trie : ";
     for (int i = 0; i < taille; i++) {
         cout << tab[i] << " ";
     }
-    cout << endl;
 
     return 0;
 }
